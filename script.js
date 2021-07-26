36a019d03efd7d81ca1f5c795b76c9ae8a3966fb
Precious John <preciousinyang10@gmail.com>
2021-07-26 16:50:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// $('.toggle').on('click', function(event) {
+//     $('.nav-bar').slideToggle('200');
+// })
+
+function toggleMenu(){
+    const Toggle=document.querySelector('.toggle');
+const navigate=document.querySelector('.nav-bar');
+menuToggle.classList.toggle('active');
+navigate.classList.toggle('active');
+}
