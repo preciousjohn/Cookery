c8a963e8eff6ad334ddf05b7195c20d3e0604a3d
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-08-04 15:18:29 +0100

    Update script.js
    I just found out I made a mistake, but has been fixed.

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const Toggle = document.querySelector('.toggle');
 const navigate = document.querySelector('.nav-bar');
 
 Toggle.addEventListener('click, function (){
-    navigate.classList,toggle('active');
+  navigate.classList.toggle('active');
 });
